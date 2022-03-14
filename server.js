1e51a9bd72979dd433c895ec4348f29d50c99f06
JorgeAguirre <joaaguir@bancolombia.com.co>
2022-03-14 10:50:25 -0500

    https

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //Install express server
+var sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
 const path = require('path');
 
 const app = express();
+
+app.use(sslRedirect());
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/'));
